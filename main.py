dfd05270cb3f413e842dfda208d8015be4edf70e
pauldg <pauldegeest1@gmail.com>
2024-02-28 17:39:27 +0100

    Added example request and response

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import List, Dict
 from closest_location import closest_destinations
 
@@ -8,13 +8,20 @@ app = FastAPI()
 
 
 class DestinationRequest(BaseModel):
-    destinations: List[dict]
-    objectstores: Dict[str, dict]
-    dataset_attributes: Dict
+    destinations: List[dict] = Field(examples=[[{"id":"pulsar_italy",
+                                                "context":{"latitude": 50.0689816,
+                                                           "longitude": 19.9070188},
+                                                "queued_job_count": 10}]])
+    objectstores: Dict[str, dict] = Field(examples=[{"object_store_italy_S3_01": {
+                                                    "latitude": 50.0689816,
+                                                    "longitude": 19.9070188,}}])
+    dataset_attributes: Dict = Field(examples=[{"dataset_italy": {
+                                                "object_store_id": "object_store_italy_S3_01",
+                                                "size": 12345678}}])
 
 
 class ProcessedResult(BaseModel):
-    sorted_destinations: List
+    sorted_destinations: List = Field(examples=[["pulsar_italy"]])
 
 
 @app.post("/process_destinations", response_model=ProcessedResult)
